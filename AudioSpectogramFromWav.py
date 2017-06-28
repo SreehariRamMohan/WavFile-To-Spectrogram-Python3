3e14876f7eec9dfffa7f0a8709247885aa1fb4ef
sreeharirammohan@gmail.com <wizmathhelp@gmail.com>
2017-06-28 15:23:06 -0700

    Added support
    Created a method to loop through all the files in a directory and save the graph as a .jpg
    
    Added methods to convert to 16-bit wav files

--- AudioSpectogramFromWav.py
+++ AudioSpectogramFromWav.py
@@ -10,103 +10,92 @@ from scipy.fftpack import fft
 from pydub import AudioSegment
 from PIL import Image
 import io
+import wave, os, glob
+import soundfile as sf
+import os
+from os.path import basename
+import soundfile
+
+
+
+def listAllWavNames(directory):
+    for file in os.listdir(directory):
+        if(file.endswith('.wav')):
+            print(file)
+
+
+def convertAllFilesInDirectoryTo16Bit(directory):
+    for file in os.listdir(directory):
+        if(file.endswith('.wav')):
+            nameSolo = basename(directory+file).rsplit('.', 1)[0]
+            print(nameSolo)
+            data, samplerate = soundfile.read(nameSolo+'.wav')
+            
+
+
+
+def batchConvertFolderOfWav(directory):
+    count = 0
+    for file in os.listdir(directory):
+        if(file.endswith('.wav')):
+            nameSolo = basename(directory+file).rsplit('.', 1)[0]
+            print('sending')
+            individualWavToSpectrogram(directory + '/' + file, nameSolo)
+            count += 1
+            print('finished ' + str(count))
+    
+def individualWavToSpectrogram(myAudio, fileNameToSaveTo):
+    print(myAudio)
+    #Read file and get sampling freq [ usually 44100 Hz ]  and sound object
+    samplingFreq, mySound = wavfile.read(myAudio)
+
+    #Check if wave file is 16bit or 32 bit. 24bit is not supported
+    mySoundDataType = mySound.dtype
+
+    #We can convert our sound array to floating point values ranging from -1 to 1 as follows
+
+    mySound = mySound / (2.**15)
+
+    #Check sample points and sound channel for duel channel(5060, 2) or  (5060, ) for mono channel
+
+    mySoundShape = mySound.shape
+    samplePoints = float(mySound.shape[0])
+
+    #Get duration of sound file
+    signalDuration =  mySound.shape[0] / samplingFreq
+
+    #If two channels, then select only one channel
+    mySoundOneChannel = mySound[:,0]
+
+    #Plotting the tone
+
+    # We can represent sound by plotting the pressure values against time axis.
+    #Create an array of sample point in one dimension
+    timeArray = numpy.arange(0, samplePoints, 1)
+
+    #
+    timeArray = timeArray / samplingFreq
+
+    #Scale to milliSeconds
+    timeArray = timeArray * 1000
+
+    #Plot the tone
+    plt.plot(timeArray, mySoundOneChannel, color='Black')
+    #plt.xlabel('Time (ms)')
+    #plt.ylabel('Amplitude')
+    print("trying to save")
+    plt.savefig('/Users/sreeharirammohan/Desktop/SavedSpecs' + fileNameToSaveTo + '.jpg')
+    print("saved")
+    plt.show()
+
+
 
 myAudio = "/Users/sreeharirammohan/Desktop/jiaaro-pydub-4abb7f2/test/data/test1.wav"
+#individualWavToSpectrogram(myAudio, "strick")
 
-#Read file and get sampling freq [ usually 44100 Hz ]  and sound object
-samplingFreq, mySound = wavfile.read(myAudio)
+directoryName = '/Users/sreeharirammohan/Desktop/UrbanSound/data/children_playing'
+#batchConvertFolderOfWav(directoryName)
 
-#Check if wave file is 16bit or 32 bit. 24bit is not supported
-mySoundDataType = mySound.dtype
+#listAllWavNames(directoryName)
 
-#We can convert our sound array to floating point values ranging from -1 to 1 as follows
-
-mySound = mySound / (2.**15)
-
-#Check sample points and sound channel for duel channel(5060, 2) or  (5060, ) for mono channel
-
-mySoundShape = mySound.shape
-samplePoints = float(mySound.shape[0])
-
-#Get duration of sound file
-signalDuration =  mySound.shape[0] / samplingFreq
-
-print("duration")
-
-
-#If two channels, then select only one channel
-mySoundOneChannel = mySound[:,0]
-
-#Plotting the tone
-
-# We can represent sound by plotting the pressure values against time axis.
-#Create an array of sample point in one dimension
-timeArray = numpy.arange(0, samplePoints, 1)
-
-#
-timeArray = timeArray / samplingFreq
-
-#Scale to milliSeconds
-timeArray = timeArray * 1000
-
-#Plot the tone
-plt.plot(timeArray, mySoundOneChannel, color='B')
-plt.xlabel('Time (ms)')
-plt.ylabel('Amplitude')
-plt.show()
-
-
-print("tone")
-
-
-#Plot frequency content
-#We can get frquency from amplitude and time using FFT , Fast Fourier Transform algorithm
-
-#Get length of mySound object array
-mySoundLength = len(mySound)
-
-#Take the Fourier transformation on given sample point 
-#fftArray = fft(mySound)
-fftArray = fft(mySoundOneChannel)
-
-numUniquePoints = numpy.ceil((mySoundLength + 1) / 2.0)
-fftArray = fftArray[0:numUniquePoints]
-
-#FFT contains both magnitude and phase and given in complex numbers in real + imaginary parts (a + ib) format.
-#By taking absolute value , we get only real part
-
-fftArray = abs(fftArray)
-
-#Scale the fft array by length of sample points so that magnitude does not depend on
-#the length of the signal or on its sampling frequency
-
-fftArray = fftArray / float(mySoundLength)
-
-#FFT has both positive and negative information. Square to get positive only
-fftArray = fftArray **2
-
-#Multiply by two (research why?)
-#Odd NFFT excludes Nyquist point
-if mySoundLength % 2 > 0: #we've got odd number of points in fft
-    fftArray[1:len(fftArray)] = fftArray[1:len(fftArray)] * 2
-
-else: #We've got even number of points in fft
-    fftArray[1:len(fftArray) -1] = fftArray[1:len(fftArray) -1] * 2  
-
-freqArray = numpy.arange(0, numUniquePoints, 1.0) * (samplingFreq / mySoundLength);
-
-#Plot the frequency
-plt.plot(freqArray/1000, 10 * numpy.log10 (fftArray), color='B')
-plt.xlabel('Frequency (Khz)')
-plt.ylabel('Power (dB)')
-
-
-#Get List of element in frequency array
-#print freqArray.dtype.type
-freqArrayLength = len(freqArray)
-print("freqArrayLength =", freqArrayLength)
-numpy.savetxt("freqData.txt", freqArray, fmt='%6.2f')
-
-#Print FFtarray information
-print("fftArray length =", len(fftArray))
-numpy.savetxt("fftData.txt", fftArray)
+convertAllFilesInDirectoryTo16Bit('/Users/sreeharirammohan/Desktop/UrbanSound/data/children_playing')
