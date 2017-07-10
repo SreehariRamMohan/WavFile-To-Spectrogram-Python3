8ed569db83e9dab4c2ebe90bd42c5df27b5e16b6
sreeharirammohan@gmail.com <wizmathhelp@gmail.com>
2017-07-10 12:11:09 -0700

    created higher resolution graphs

--- AudioSpectogramFromWav.py
+++ AudioSpectogramFromWav.py
@@ -2,6 +2,13 @@
 Time in MS Vs Amplitude in DB of a input wav signal
 """
 
+#current bug: spectrogram images are repeated, they have titles like carhorn1923416BIT16BIT
+#so the 16 bit gets repeated and the program re-reads them so I need to somehow figure out  how to fix this.
+
+#current bug: The graphs scales aren't small enough. As a result, all of my graphs end up looking like the same
+#black/grey rectangle. 
+
+
 import numpy
 import matplotlib.pyplot as plt
 import pylab
@@ -17,27 +24,28 @@ from os.path import basename
 import soundfile
 
 
-
 def listAllWavNames(directory):
     for file in os.listdir(directory):
-        if(file.endswith('.wav')):
+        if(file.endswith('.wav') and 'normal' in file):
             print(file)
+
 
 
 def convertAllFilesInDirectoryTo16Bit(directory):
     for file in os.listdir(directory):
         if(file.endswith('.wav')):
+            print('file is ' + file)
             nameSolo = file.rsplit('.', 1)[0]
             print(directory + nameSolo )
             data, samplerate = soundfile.read(directory + file)                
-            soundfile.write('/Users/sreeharirammohan/Desktop/UrbanSound/data/street_music/' + nameSolo + '16BIT.wav', data, samplerate, subtype='PCM_16')
+            soundfile.write('/Users/sreeharirammohan/Desktop/heartbeat-16bit/' + nameSolo + '|16|.wav', data, samplerate, subtype='PCM_16')
             print("converting " + file + "to 16 - bit")
             
 
 def batchConvertFolderOfWav(directory):
     count = 0
     for file in os.listdir(directory):
-        if(file.endswith('16BIT.wav')):
+        if(file.endswith('.wav')):
             nameSolo = basename(directory+file).rsplit('.', 1)[0]
             print('sending')
             individualWavToSpectrogram(directory + '/' + file, nameSolo)
@@ -84,24 +92,29 @@ def individualWavToSpectrogram(myAudio, fileNameToSaveTo):
 
     #Scale to milliSeconds
     timeArray = timeArray * 1000
+
     plt.rcParams['agg.path.chunksize'] = 100000
+
+    
     #Plot the tone
+    plt.xlim((0, 8000))
+    plt.figure(figsize=(35,15))
     plt.plot(timeArray, mySoundOneChannel, color='Black')
     #plt.xlabel('Time (ms)')
     #plt.ylabel('Amplitude')
     print("trying to save")
-    plt.savefig('/Users/sreeharirammohan/Desktop/SavedUrbanSpectrograms/air_conditioner/' + fileNameToSaveTo + '.jpg')
+    plt.savefig('/Users/sreeharirammohan/Desktop/' + fileNameToSaveTo + '.jpg')
     print("saved")
     print("not going to show now")
     #plt.show()
     #plt.close()
 
-#myAudio = "/Users/sreeharirammohan/Desktop/jiaaro-pydub-4abb7f2/test/data/test1.wav"
-#individualWavToSpectrogram(myAudio, "strick")
+myAudio = "/Users/sreeharirammohan/Desktop/heartbeat-sounds/set_a/murmur__201108222258.wav"
+individualWavToSpectrogram(myAudio, "strick10")
 
-directoryName = '/Users/sreeharirammohan/Desktop/UrbanSound/data/air_conditioner'
-batchConvertFolderOfWav(directoryName)
+#directoryName = '/Users/sreeharirammohan/Desktop/heartbeat-sounds/set_a'
+#batchConvertFolderOfWav(directoryName)
 
 #listAllWavNames(directoryName)
 
-#convertAllFilesInDirectoryTo16Bit('/Users/sreeharirammohan/Desktop/UrbanSound/data/street_music/')
+#convertAllFilesInDirectoryTo16Bit('/Users/sreeharirammohan/Desktop/heartbeat-sounds/set_b')
